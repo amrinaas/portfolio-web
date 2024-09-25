6cc34011dd5246a34ade16b913176e5034364d56
Amrina Aulia Siregar <amrinaas96@gmail.com>
2024-09-25 07:36:00 +0700

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        vt: ['VT323', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
