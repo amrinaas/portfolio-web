7c1e0d7a58d7ca25732a21a52e678255d64aeeeb
Amrina Aulia Siregar <amrinaas96@gmail.com>
2024-09-25 08:10:41 +0700

    Add title component

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = ({ title }) => {
+  return (
+    <div>
+      <h1 className='uppercase text-5xl'>
+        <span className='text-rose-700'>&lt; </span>
+        {title}
+        <span className='text-rose-700'> /&gt;</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
